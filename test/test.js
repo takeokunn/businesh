bb6045b9a28ccf927f6fa07310936f47ec26e231
takeokunn <bararararatty@gmail.com>
2019-02-17 23:35:09 +0900

    Update

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 const test = require('ava');
-const businesh = require('../dist/bundle');
+const businesh = require('../dist/bundle.umd');
 const before_text = '任意の文字列をビジネッシュ・テキストに変換するライブラリです。';
 
 test('test', t => businesh.translate(before_text).then(after_text => t.not(before_text, after_text)));
